dd4fb56c792e8f1b8e7f0cc9f7a6b1ec60513b07
siso <siso25@zoho.com>
2022-01-22 13:41:52 +0900

    牌の組み合わせを作る処理追加
    渡された牌で、3個ずつの組み合わせを作る処理を追加。

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import Score from './lib/score.js'
 
-const score = new Score('1r,2,3,c,c,c')
-const tiles = score.calculate()
-console.log(tiles)
+const score = new Score('1r,1,6,1,4,5')
+score.calculate()
