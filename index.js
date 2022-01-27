d21f3d5e504acc474dc81b234be1741b1b636eb7
siso <siso25@zoho.com>
2022-01-27 23:57:30 +0900

    変数名を修正
    配列の長さの最大数を格納する変数名を変更

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dora = '1'
 // const tiles = ['1', '1', '2', '4', '5', '6']
 // const tiles = ['1', '2', '2', '2', '2', '3']
 // const tiles = ['1', '1', '1', '1', '2', '3']
-const tiles = ['1', '2', '3', '3', '3', '3']
+// const tiles = ['1', '2', '3', '3', '3', '3']
+const tiles = ['1', '2', '3', '4', '5', '6']
 const score = new Score(numberOfPeople, dora, tiles)
 console.log(score.calculate())
