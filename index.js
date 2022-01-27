dd1bc92236de7e16ced961ea587e014b157f53e6
siso <siso25@zoho.com>
2022-01-27 23:41:29 +0900

    面子の組み合わせ作成メソッド修正
    順子2組の場合、同じ牌が4つの場合に対応

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import Score from './lib/score.js'
 
 const numberOfPeople = 3
 const dora = '1'
-const tiles = ['1r', '1', '5', '1', '4', '5']
+// const tiles = ['1', '1', '1', '3', '3', '3']
+// const tiles = ['1', '1', '2', '2', '3', '3']
+// const tiles = ['1', '1', '2', '4', '5', '6']
+// const tiles = ['1', '2', '2', '2', '2', '3']
+// const tiles = ['1', '1', '1', '1', '2', '3']
+const tiles = ['1', '2', '3', '3', '3', '3']
 const score = new Score(numberOfPeople, dora, tiles)
 console.log(score.calculate())
