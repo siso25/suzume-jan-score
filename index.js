beef7fcaf803fc1eb025a19334f312d4c8245483
siso <siso25@zoho.com>
2022-01-30 21:25:56 +0900

    mochaでテストできないのでインポート方法をrequireに変更
    import構文を使うとmochaでテストするのが大変なので、
    requireを使用するように変更

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import Score from './lib/score.js'
+const Score = require('./lib/score.js')
 
-const numberOfPeople = 3
+// const numberOfPeople = 3
+// const isLeader = true
 const dora = '1'
 // const tiles = ['1', '1', '1', '3', '3', '3']
 // const tiles = ['1', '1', '2', '2', '3', '3']
@@ -9,6 +10,6 @@ const dora = '1'
 // const tiles = ['1', '1', '1', '1', '2', '3']
 // const tiles = ['1', '2', '3', '3', '3', '3']
 // const tiles = ['1', '2', '3', '4', '5', '6']
-const tiles = ['1', '1', '1r', 'h', 'h', 'h']
-const score = new Score(numberOfPeople, dora, tiles)
+const tiles = ['1', '1', '1r', 'gd', 'gd', 'gd']
+const score = new Score(dora, tiles)
 console.log(score.calculate())
